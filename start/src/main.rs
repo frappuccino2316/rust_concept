abf2772287e72614aaca30040dffdd9ca0084481
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-14 01:07:56 +0900

    create a function to divide

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    ex_print_some(ex_div_some(15, 5))
+}
+
+fn ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(x) = ans {
+        println!("{}", x)
+    } else {
+        println!("None")
+    }
+}
+
+fn ex_div_some(x: i32, y: i32) -> Option<i32> {
+    if y == 0 {
+        None
+    } else {
+        Some(x / y)
+    }
 }
