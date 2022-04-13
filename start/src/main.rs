b0899d6d9593f15ac5baa08918b89fc3a205667b
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-14 01:15:24 +0900

    add result pattern

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    ex_print_some(ex_div_some(15, 5))
+    ex_print_some(ex_div_some(15, 5));
+    ex_print_result(ex_div_result(10, 5));
 }
 
 fn ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
@@ -10,10 +11,25 @@ fn ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
     }
 }
 
+fn ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
+    match ans {
+        Ok(res) => println!("{}", res),
+        Err(str) => println!("{}", str),
+    }
+}
+
 fn ex_div_some(x: i32, y: i32) -> Option<i32> {
     if y == 0 {
         None
     } else {
         Some(x / y)
+    }
+}
+
+fn ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
+    if y == 0 {
+        Err("div by zero")
+    } else {
+        Ok(x / y)
     }
 }
