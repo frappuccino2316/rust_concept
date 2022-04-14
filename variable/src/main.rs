a0dce4017a8152e15574267a817b451808757304
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-15 00:38:58 +0900

    cannot create many mutable reference

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let s = "Hello".to_string();
-    myprint(&s);
-    myprint(&s);
+    let mut s = "Hello".to_string();
+    println!("s={}", s);
+
+    let s_ref = &mut s;
+    myclear(s_ref);
+    println!("s={}", s);
 }
 
-fn myprint<T: std::fmt::Display>(msg: &T) {
-    println!("{}", *msg);
+fn myclear(x: &mut String) {
+    x.clear();
 }
