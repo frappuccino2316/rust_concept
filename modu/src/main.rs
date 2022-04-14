92a95212bcba837cf31a847951782124371948df
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-14 19:18:34 +0900

    use function

--- modu/src/main.rs
+++ modu/src/main.rs
@@ -1,7 +1,7 @@
 mod module_a;
-use crate::module_a::module_b;
+use crate::module_a::module_b::print_yoroshiku;
 
 fn main() {
     crate::module_a::print_hello();
-    module_b::print_yoroshiku();
+    print_yoroshiku();
 }
