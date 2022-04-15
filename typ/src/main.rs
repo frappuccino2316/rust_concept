529f920d6042a568776b96abcecd498e58da816f
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-15 23:56:25 +0900

    list

--- typ/src/main.rs
+++ typ/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let ii = 1_i8;
-    let jj = 2_u16;
-    let kk: i32 = ii as i32 + jj as i32;
-    println!("{}", kk);
+    let ary = [0, 1, 2, 3];
+    for aa in &ary {
+        println!("{}", aa);
+    }
+    println!("{}", ary[1])
 }
