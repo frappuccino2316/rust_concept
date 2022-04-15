4396cbc424e7d07407599fd7f07678eb622c3ed3
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-15 12:51:15 +0900

    リファレンスのスコープと重なっているので更新できない

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,16 +1,11 @@
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("{}", s);
+    let mut x = 1;
+    let x_ref = &x;
 
-    let s_ref = &mut s;
-    myclear(s_ref);
-    println!("{}", s);
-
-    let s_ref2 = &mut s;
-    myclear(s_ref2);
-    println!("{}", s);
+    x = 2;
+    println!("{}", x_ref);
 }
 
-fn myclear(x: &mut String) {
-    x.clear();
-}
+// fn myclear(x: &mut String) {
+//     x.clear();
+// }
