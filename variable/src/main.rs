f2dd856324cd454f4bc7353bfc017be364561b4a
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-15 12:38:16 +0900

    format :p for print address

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,13 +1,4 @@
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("s={}", s);
-
-    let s_ref = &mut s;
-    let ss_ref = &mut s;
-    myclear(s_ref);
-    println!("s={}", s);
-}
-
-fn myclear(x: &mut String) {
-    x.clear();
+    let s = "Hello".to_string();
+    println!("{:p}", &s);
 }
