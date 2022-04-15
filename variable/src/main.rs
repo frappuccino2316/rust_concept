48cbbbfed18ec76c9749e2b15cc5432bec49149e
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-15 12:45:48 +0900

    can declare many mutable reference for deff scope

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    let s = "Hello".to_string();
-    println!("{:p}", &s);
+    let mut s = "Hello".to_string();
+    println!("{}", s);
+
+    let s_ref = &mut s;
+    myclear(s_ref);
+    println!("{}", s);
+
+    let s_ref2 = &mut s;
+    myclear(s_ref2);
+    println!("{}", s);
+}
+
+fn myclear(x: &mut String) {
+    x.clear();
 }
