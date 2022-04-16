81be31c94b18c79f4452a14fe631e083d336b54e
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-16 23:31:38 +0900

    struct in struct

--- struct_type/src/main.rs
+++ struct_type/src/main.rs
@@ -1,15 +1,24 @@
 fn main() {
-    let mut taro = person::Person::new(String::from("taro"), 10);
+    // let mut taro = person::Person::new(String::from("taro"), 10);
 
-    println!("{}", taro.name);
+    // println!("{}", taro.name);
 
-    let age_plus1 = taro.age_incr(1);
-    println!("{}", age_plus1);
+    // let age_plus1 = taro.age_incr(1);
+    // println!("{}", age_plus1);
 
-    taro.age_incr_replace(10);
-    println!("{}", taro.age);
+    // taro.age_incr_replace(10);
+    // println!("{}", taro.age);
 
-    println!("{:?}", taro);
+    // println!("{:?}", taro);
+
+    let taro = person::Person::new(String::from("taro"), 50);
+    let hanako = person::Person::new(String::from("hanako"), 48);
+
+    let sato = Parents {
+        father: taro,
+        mother: hanako,
+    };
+    println!("{:?}", sato);
 }
 
 mod person {
@@ -24,12 +33,18 @@ mod person {
             Person { name, age }
         }
 
-        pub fn age_incr(&self, incr: u8) -> u8 {
-            self.age + incr
-        }
+        // pub fn age_incr(&self, incr: u8) -> u8 {
+        //     self.age + incr
+        // }
 
-        pub fn age_incr_replace(&mut self, incr: u8) {
-            self.age += incr;
-        }
+        // pub fn age_incr_replace(&mut self, incr: u8) {
+        //     self.age += incr;
+        // }
     }
+}
+
+#[derive(Debug)]
+struct Parents {
+    father: person::Person,
+    mother: person::Person,
 }
