33f5a1e8af9983b817e23402db966136e99bfe23
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-17 23:05:36 +0900

    enum

--- enu/src/main.rs
+++ enu/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn determine_sign(x: i32) -> Sign {
+    if x > 0 {
+        Sign::Positive
+    } else if x < 0 {
+        Sign::Negative
+    } else {
+        Sign::Zero
+    }
+}
+
+enum Sign {
+    Positive,
+    Zero,
+    Negative
+}
