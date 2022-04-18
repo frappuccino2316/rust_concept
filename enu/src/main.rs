a03d14e8a5340ab00540ff275ccba7643fe7ad26
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-18 20:38:24 +0900

    data in enum

--- enu/src/main.rs
+++ enu/src/main.rs
@@ -1,29 +1,36 @@
-use std::cmp::Ordering;
-
 fn main() {
-    print_sign(determine_sign(1));
-    print_sign(determine_sign(-2));
-    print_sign(determine_sign(0));
-}
+    let v: Vec<EnumExample> = vec![
+        EnumExample::TupleTypeExample1(String::from("Hello")),
+        EnumExample::TupleTypeExample2(10, true),
+        EnumExample::StructTypeExample {
+            name: String::from("taro"),
+            age: 10,
+        },
+    ];
 
-fn determine_sign(x: i32) -> Sign {
-    match x.cmp(&0) {
-        Ordering::Greater => Sign::Positive,
-        Ordering::Less => Sign::Negative,
-        Ordering::Equal => Sign::Zero
+    for e in &v {
+        if let EnumExample::StructTypeExample { name: n, age: a } = e {
+            println!("StructTypeExample_iflet: name = {}, age = {}", n, a);
+        }
+    }
+
+    for e in &v {
+        match e {
+            EnumExample::TupleTypeExample1(s) => {
+                println!("TupleTypeExample1: s = {}", s);
+            }
+            EnumExample::TupleTypeExample2(i, b) => {
+                println!("TupleTypeExample2: i = {}, b = {}", i, b);
+            }
+            EnumExample::StructTypeExample { name: n, .. } => {
+                println!("StructTypeExample: name = {}", n);
+            }
+        }
     }
 }
 
-fn print_sign(s: Sign) {
-    match s {
-        Sign::Positive => println!("+"),
-        Sign::Zero => println!("0"),
-        Sign::Negative => println!("-"),
-    }
-}
-
-enum Sign {
-    Positive,
-    Zero,
-    Negative,
+enum EnumExample {
+    TupleTypeExample1(String),
+    TupleTypeExample2(i32, bool),
+    StructTypeExample { name: String, age: u8 },
 }
