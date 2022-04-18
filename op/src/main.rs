367d1c90308934a5e2d7192f5721a6ad35dd577b
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-18 20:48:15 +0900

    option

--- op/src/main.rs
+++ op/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    if let Some(result) = func_ex_div_some(8, 2) {
+        println!("{}", result);
+    } else {
+        println!("None");
+    }
+}
+
+fn func_ex_div_some(x: i32, y: i32) -> Option<i32> {
+    if y == 0 {
+        None
+    } else {
+        Some(x / y)
+    }
 }
