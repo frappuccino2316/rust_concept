c4addc400d654ec4971cc1984afbf185b461e678
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-18 23:39:29 +0900

    use is_ok method

--- typ/src/main.rs
+++ typ/src/main.rs
@@ -7,9 +7,11 @@ fn main() {
 }
 
 fn print_mydiv(x: i32, y: i32) {
-    match mydiv(x, y) {
-        Ok(ans) => println!("no error. ans = {}", ans),
-        Err(e) => println!("{}", e),
+    let ret = mydiv(x, y);
+    if ret.is_ok() {
+        println!("no error. ans = {}", ret.unwrap());
+    } else {
+        println!("{}", ret.err().unwrap());
     }
 }
 
