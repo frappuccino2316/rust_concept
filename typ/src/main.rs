ea64495d412de19f1e8fe30d99aba4e2c684147a
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-18 21:57:15 +0900

    Result

--- typ/src/main.rs
+++ typ/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
-    let mut s = "Hello".to_string();
-    let ss = String::from("Hello!!");
+    func_ex_print_result(func_ex_div_result(10, 5));
+    func_ex_print_result(func_ex_div_result(10, 0));
+}
 
-    s.push_str(", World!");
+fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
+    if y == 0 {
+        Err("dic by zero")
+    } else {
+        Ok(x / y)
+    }
+}
 
-    println!("{}", s);
-    println!("{}", ss);
-    println!("{}", &s[0..3]);
+fn func_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
+    match ans {
+        Ok(res) => println!("{}", res),
+        Err(str) => println!("{}", str),
+    }
 }
