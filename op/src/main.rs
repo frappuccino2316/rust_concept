2c780eb9ff73a0046147ba25a73d11bbf87427cc
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-18 21:00:18 +0900

    use expect

--- op/src/main.rs
+++ op/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let rr1 = func_ex_unwrap(5);
-    println!("rr1: {}", rr1.unwrap());
+    println!("rr1: {}", rr1.expect("Panic 1"));
 
     let rr2 = func_ex_unwrap(-5);
-    println!("rr1: {}", rr2.unwrap());
+    println!("rr1: {}", rr2.expect("Panic 2"));
 }
 
 fn func_ex_unwrap(x: i32) -> Option<i32> {
