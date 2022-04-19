e942405d925954a339fd860273da4daafec2d1b2
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-20 01:10:44 +0900

    use Rc

--- typ/src/main.rs
+++ typ/src/main.rs
@@ -1,23 +1,36 @@
-const BUFSIZE: usize = 1024;
-static OFFSET: usize = 15;
+use std::rc::Rc;
 
 fn main() {
-    let offset_ref = &OFFSET;
+    let mut data_a_1 = DataA { number_a: None };
+    let mut data_b_1 = DataB { number_b: None };
+    let mut data_a_2 = DataA { number_a: None };
+    let mut data_b_2 = DataB { number_b: None };
 
-    println!("bufsize = {}", BUFSIZE);
-    println!("offset = {}", offset_ref);
+    setdata(&mut data_a_1, &mut data_b_1, 1);
+    setdata(&mut data_a_2, &mut data_b_2, 2);
 
-    add_static();
-    add_static();
-    add_static();
+    println!(
+        "to be 11, 11: {}, {}",
+        data_a_1.number_a.unwrap(),
+        data_b_1.number_b.unwrap()
+    );
+    println!(
+        "to be 12, 12: {}, {}",
+        data_a_2.number_a.unwrap(),
+        data_b_2.number_b.unwrap()
+    );
 }
 
-fn add_static() {
-    const INCREMENT: usize = 2;
-    static mut ADD: usize = 1;
+fn setdata(data_a: &mut DataA, data_b: &mut DataB, value: i32) {
+    let number = Rc::new(value + 10);
+    data_a.number_a = Some(Rc::clone(&number));
+    data_b.number_b = Some(Rc::clone(&number));
+}
 
-    unsafe {
-        ADD += INCREMENT;
-        println!("add = {}", ADD);
-    }
+struct DataA {
+    number_a: Option<Rc<i32>>,
+}
+
+struct DataB {
+    number_b: Option<Rc<i32>>,
 }
