19cf3f05c6d8836d1d9acc3dc270e8fb8eeb9fce
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-19 22:33:55 +0900

    use ? in main

--- typ/src/main.rs
+++ typ/src/main.rs
@@ -1,35 +1,40 @@
 use thiserror::Error;
 
-fn main() {
-    println!("1st calc");
-    print_repeat_mydiv(repeat_mydiv(&[(2, 1), (9, 3)]));
-    println!();
-
-    println!("2nd calc");
-    print_repeat_mydiv(repeat_mydiv(&[(2, 1), (-6, -3), (5, 2)]));
-    println!();
-
-    println!("3rd calc");
-    print_repeat_mydiv(repeat_mydiv(&[(2, 1), (-6, 0), (6, 3)]));
-    println!();
+fn main() -> Result<(), DivError> {
+    println!("{}", mydiv(3, 2)?);
+    Ok(())
 }
 
-fn print_repeat_mydiv(result: Result<Vec<i32>, DivError>) {
-    match result {
-        Ok(v) => println!("{:?}", v),
-        Err(e) => println!("{}", e),
-    }
-}
+// fn main() {
+//     println!("1st calc");
+//     print_repeat_mydiv(repeat_mydiv(&[(2, 1), (9, 3)]));
+//     println!();
 
-fn repeat_mydiv(ary: &[(i32, i32)]) -> Result<Vec<i32>, DivError> {
-    let mut ret = Vec::new();
-    for aa in ary {
-        let ans = mydiv(aa.0, aa.1)?;
-        ret.push(ans);
-        println!("pushed: {} / {} = {}", aa.0, aa.1, ans);
-    }
-    Ok(ret)
-}
+//     println!("2nd calc");
+//     print_repeat_mydiv(repeat_mydiv(&[(2, 1), (-6, -3), (5, 2)]));
+//     println!();
+
+//     println!("3rd calc");
+//     print_repeat_mydiv(repeat_mydiv(&[(2, 1), (-6, 0), (6, 3)]));
+//     println!();
+// }
+
+// fn print_repeat_mydiv(result: Result<Vec<i32>, DivError>) {
+//     match result {
+//         Ok(v) => println!("{:?}", v),
+//         Err(e) => println!("{}", e),
+//     }
+// }
+
+// fn repeat_mydiv(ary: &[(i32, i32)]) -> Result<Vec<i32>, DivError> {
+//     let mut ret = Vec::new();
+//     for aa in ary {
+//         let ans = mydiv(aa.0, aa.1)?;
+//         ret.push(ans);
+//         println!("pushed: {} / {} = {}", aa.0, aa.1, ans);
+//     }
+//     Ok(ret)
+// }
 
 fn mydiv(x: i32, y: i32) -> Result<i32, DivError> {
     if y == 0 {
