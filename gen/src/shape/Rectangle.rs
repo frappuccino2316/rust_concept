ff05170453ccefaa5c1dccb189df4797bb31ff56
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-21 20:58:58 +0900

    separate file

---
+++ gen/src/shape/Rectangle.rs
@@ -0,0 +1,12 @@
+use crate::calc::area::CalcArea;
+
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f64 {
+        self.width * self.height
+    }
+}
