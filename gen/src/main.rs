597eb0e96a70c5d152270d84773609a29302c08e
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-21 23:12:49 +0900

    RightTriangleも出力

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,8 +1,10 @@
 mod calc;
 mod rectangle;
+mod right_triangle;
 
 use calc::CalcArea;
 use rectangle::Rectangle;
+use right_triangle::RightTriangle;
 
 fn main() {
     let rect = Rectangle {
@@ -10,6 +12,12 @@ fn main() {
         height: 2.0,
     };
     println!("{}", area(&rect));
+
+    let right = RightTriangle {
+        width: 3.0,
+        height: 8.0,
+    };
+    println!("{}", area(&right));
 }
 
 fn area<T: CalcArea>(x: &T) -> f64 {
