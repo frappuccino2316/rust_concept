39e443b321025b97b24f51815380be2857589e62
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-21 23:23:41 +0900

    fix

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -2,24 +2,20 @@ mod calc;
 mod rectangle;
 mod right_triangle;
 
-use calc::CalcArea;
-use rectangle::Rectangle;
-use right_triangle::RightTriangle;
-
 fn main() {
-    let rect = Rectangle {
+    let rect = rectangle::Rectangle {
         width: 1.0,
         height: 2.0,
     };
     println!("{}", area(&rect));
 
-    let right = RightTriangle {
+    let right = right_triangle::RightTriangle {
         width: 3.0,
         height: 8.0,
     };
     println!("{}", area(&right));
 }
 
-fn area<T: CalcArea>(x: &T) -> f64 {
+fn area<T: calc::CalcArea>(x: &T) -> f64 {
     x.calc_area()
 }
