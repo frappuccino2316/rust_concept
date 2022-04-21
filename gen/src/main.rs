0433e683e12f2b606d4b5729f60bc19cc24d41a3
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-21 23:53:22 +0900

    add use

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -2,20 +2,24 @@ mod calc;
 mod rectangle;
 mod right_triangle;
 
+use calc::CalcArea;
+use rectangle::Rectangle;
+use right_triangle::RightTriangle;
+
 fn main() {
-    let rect = rectangle::Rectangle {
+    let rect = Rectangle {
         width: 1.0,
         height: 2.0,
     };
     println!("{}", area(&rect));
 
-    let right = right_triangle::RightTriangle {
+    let right = RightTriangle {
         width: 3.0,
         height: 8.0,
     };
     println!("{}", area(&right));
 }
 
-fn area<T: calc::CalcArea>(x: &T) -> f64 {
+fn area<T: CalcArea>(x: &T) -> f64 {
     x.calc_area()
 }
