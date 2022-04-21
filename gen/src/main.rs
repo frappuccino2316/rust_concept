a56336da898285bf069d162d75d2e2f4944c6b35
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-21 12:30:13 +0900

    use Generics

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x1 = return_input(1);
+    let x2 = return_input(String::from("Hello World"));
+    let x3 = return_input::<f64>(2.0);
+
+    println!("{}", x1);
+    println!("{}", x2);
+    println!("{}", x3);
+}
+
+fn return_input<T>(x: T) -> T {
+    x
 }
