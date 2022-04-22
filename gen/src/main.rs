52b6443e31c62d407294229617b0735d3f07e802
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-22 22:56:30 +0900

    use line

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -8,6 +8,8 @@
 mod shape;
 
 use shape::calc::CalcArea;
+use shape::calc::CalcLength;
+use shape::line::Line;
 use shape::rectangle::Rectangle;
 use shape::right_triangle::RightTriangle;
 
@@ -17,12 +19,17 @@ fn main() {
         height: 2.0,
     };
     println!("{}", area(&rect));
+    println!("{}", length(&rect));
 
     let right = RightTriangle {
         width: 3.0,
         height: 8.0,
     };
     println!("{}", area(&right));
+    println!("{}", length(&right));
+
+    let line = Line { length: 10.0 };
+    println!("{}", length(&line));
 }
 
 fn area<T>(x: &T) -> f64
@@ -30,4 +37,8 @@ where
     T: CalcArea,
 {
     x.calc_area()
+}
+
+fn length<T: CalcLength>(x: &T) -> f64 {
+    x.calc_length()
 }
