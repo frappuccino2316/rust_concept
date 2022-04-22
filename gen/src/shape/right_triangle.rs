1ae8e52e5554e61351ec4770e4f07e9be48d7627
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-22 22:51:18 +0900

    add calc_length in RightTriangle

--- gen/src/shape/right_triangle.rs
+++ gen/src/shape/right_triangle.rs
@@ -1,4 +1,5 @@
 use crate::shape::calc::CalcArea;
+use crate::shape::calc::CalcLength;
 
 pub struct RightTriangle {
     pub width: f64,
@@ -8,5 +9,11 @@ pub struct RightTriangle {
 impl CalcArea for RightTriangle {
     fn calc_area(&self) -> f64 {
         self.width * self.height * 0.5
+    }
+}
+
+impl CalcLength for RightTriangle {
+    fn calc_length(&self) -> f64 {
+        self.width + self.height + (self.width.powi(2) + self.height.powi(2)).sqrt()
     }
 }
