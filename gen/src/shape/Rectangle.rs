8a967b8e0ff00c7dcee45b04a26ab09b829af0e8
frappuccino2316 <frappuccino2316@gmail.com>
2022-04-22 22:46:58 +0900

    add calc_length in Rectangle

--- gen/src/shape/Rectangle.rs
+++ gen/src/shape/Rectangle.rs
@@ -1,4 +1,5 @@
 use crate::shape::calc::CalcArea;
+use crate::shape::calc::CalcLength;
 
 pub struct Rectangle {
     pub width: f64,
@@ -8,5 +9,11 @@ pub struct Rectangle {
 impl CalcArea for Rectangle {
     fn calc_area(&self) -> f64 {
         self.width * self.height
+    }
+}
+
+impl CalcLength for Rectangle {
+    fn calc_length(&self) -> f64 {
+        (self.width + self.height) * 2.0
     }
 }
